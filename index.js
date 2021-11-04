465f9b558aa1fc48d0fbf0679d6ffb7a9bcc10b3
Alfonso Martínez <alfmar100391@icloud.com>
2021-11-03 19:26:48 -0600

    First commit

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 //Imports de los módulos
 //fs y path
+const fs = require('fs/promises');
+const path = require('path');
 
 const getUsers = async() => {
     //Leer el archivo
 
     //Regresar el arreglo de usuarios como objeto literal Javascript (sin notación JSON)
+    try {
+        const pathUser = path.resolve("users.json");
+        const users = await fs.readFile(pathUser, {encoding:'utf8'});
+        console.log(JSON.parse(users));
+        return JSON.parse(users);
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 const addUser = async (userObj) => {
@@ -18,13 +28,31 @@ const addUser = async (userObj) => {
 
     //si el proceso se realizó correctamente tendrás que regresar el objeto de usuario
     //que acabas de agregar en el arreglo
+    try {
+        const users = await getUsers();
+        users.push(userObj);
+        const pathUsers = path.resolve("users.json");
+        await fs.writeFile(pathUsers, JSON.stringify(users));
+        return userObj;
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 const clearUsers = async () => {
     //Vaciar el arreglo y sobreescribir el archivo
     
     //Si el proceso se realizó correctamente tendrás que regresar true
+    try {
+        const pathUsers = path.resolve("users.json");
+        await fs.writeFile(pathUsers, JSON.stringify([]));
+        return true;
+    } catch (error) {
+        console.error(error);
+    }
 }
+
+getUsers();
 
 module.exports = {
     getUsers,
